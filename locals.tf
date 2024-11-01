f696e9bb6b4bd5afabefe2787f02c99758b4eee1
Jithendar.Kota <jithendar.kota@miba.com>
2024-11-01 20:17:04 +0530

    pushing the code

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
     US = "East US"
   }
   resource_group_name = {
-    webapp_europe = "rg-webapp-test-westeurope-001"
+    webapp_europe = "rg-test-practice-tf-001"
     # webapp_us = "rg-webapp-${var.stage}-eastus-001"
     # sqlserver_europe = "rg-sql-${var.stage}-westeurope-001"
     # sqlserver_us = "rg-sql-${var.stage}-eastus-001"
